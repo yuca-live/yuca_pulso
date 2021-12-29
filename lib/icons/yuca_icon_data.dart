7b969bbe5b6208bff4d1dad23565a5f586bdcc52
Rodrigo-yuca <rodrigo.amancio@yuca.live>
2021-12-29 17:55:44 -0300

    adjust code to work icons

---
+++ lib/icons/yuca_icon_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+@immutable
+class YucaIconsData extends IconData {
+  const YucaIconsData(int codePoint) : super(codePoint, fontFamily: 'YucaIcons', fontPackage: 'yuca_pulso');
+}
--- lib/yuca_pulso.dart
+++ lib/yuca_pulso.dart
@@ -3,3 +3,4 @@ library yuca_pulso;
 export 'typography/typography.dart';
 export 'typography/text_styles.dart';
 export 'colors/color_styles.dart';
+export 'icons/yuca_icons.dart';
