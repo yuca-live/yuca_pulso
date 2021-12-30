099de61d3ad7c8165a2027223359e99861f419c2
Will Xavier <willyelns@yuca.live>
2021-12-30 16:20:57 -0300

    refactoring: update the lib index file

--- lib/yuca_pulso.dart
+++ lib/yuca_pulso.dart
@@ -1,6 +1,6 @@
 library yuca_pulso;
 
-export 'typography/typography.dart';
-export 'typography/text_styles.dart';
-export 'colors/color_styles.dart';
-export 'icons/yuca_icons.dart';
+export './colors/color_styles.dart';
+export './icons/yuca_icons.dart';
+export './typography/typography.dart';
+export './utils/utils.dart';
