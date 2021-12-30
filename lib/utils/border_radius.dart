fafe16ae8645fda3503d2771cb0517e0351abf79
Will Xavier <willyelns@yuca.live>
2021-12-30 16:01:46 -0300

    feature: Add BorderRadius control list

---
+++ lib/utils/border_radius.dart
@@ -0,0 +1,27 @@
+/// A [BorderRadius] factory to creates all the Design System's listed BorderRadius values.
+/// We set the class into the abstract mode to prevent the user creates an instance without value,
+/// but keeping the support to be extended in the future.
+
+library yuca_pulso;
+
+import 'package:flutter/material.dart';
+
+abstract class YucaBorderRadius extends BorderRadius {
+  /// Returns a [BorderRadius] with the `small` size
+  const YucaBorderRadius.small() : super.all(const Radius.circular(4));
+
+  /// Returns a [BorderRadius] with the `medium` size
+  const YucaBorderRadius.medium() : super.all(const Radius.circular(6));
+
+  /// Returns a [BorderRadius] with the `large` size
+  const YucaBorderRadius.large() : super.all(const Radius.circular(8));
+
+  /// Returns a [BorderRadius] with the `extra large` size
+  const YucaBorderRadius.extraLarge() : super.all(const Radius.circular(10));
+
+  /// Returns a [BorderRadius] with a `complete Circle`
+  /// It will depend on the Object Size. If it has a big size, probably
+  /// we will have to find another way to transform it into a circle.
+  /// A better approach should be to use the [Shape] property.
+  const YucaBorderRadius.full() : super.all(const Radius.circular(100));
+}
