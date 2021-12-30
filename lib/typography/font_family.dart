c7bf6ab89c8e4cedfafb433991e2bc496e341620
Will Xavier <willyelns@yuca.live>
2021-12-30 16:13:52 -0300

    doc: Add some docs to YucaFontFamily

--- lib/typography/font_family.dart
+++ lib/typography/font_family.dart
@@ -1,6 +1,20 @@
-/// All the possible FontFamily that can be used to set the TextStyle
+/// List the `Font Families` supported by the Design System
+library yuca_pulso;
 
-class FontFamily {
-  static const frauncesFont = 'Fraunces';
-  static const montserratFont = 'Montserrat';
+class YucaFontFamily {
+  /// By creating a empty private constructor, we prevent the user from extends
+  ///  or creates an empty instance from this class.
+  YucaFontFamily._();
+
+  /// Used in some Headlines texts
+  static const fraunces = 'Fraunces';
+
+  /// Basically used in all the texts.
+  static const montserrat = 'Montserrat';
+
+  /// Returns a list with all the supported font families.
+  /// To use in a [TextStyle] or [AppTheme], please check out the font family attribute itself.
+  /// Example:
+  /// Text('data', style: TextStyle(fontFamily: FontFamily.frauncesFont));
+  static const list = [fraunces, montserrat];
 }
