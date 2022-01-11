7d2ac2875fd2e0788ce5035e16c5a61509b9adec
Will Xavier <willyelns@yuca.live>
2022-01-10 22:59:17 -0300

    feat: update typography enum names

--- lib/typography/text_styles.dart
+++ lib/typography/text_styles.dart
@@ -1,23 +1,123 @@
 /// All the default TextStyle possibilities defined by the Design System
 library yuca_pulso;
 
-// ignore_for_file: constant_identifier_names
 enum YucaTextStyles {
-  H0,
-  H1,
-  H2,
-  H3,
-  H4,
-  H5,
-  BODY_LARGE_BOLD,
-  BODY_LARGE_REGULAR,
-  BODY_MEDIUM_BOLD,
-  BODY_MEDIUM_REGULAR,
-  BODY_SMALL_BOLD,
-  BODY_SMALL_REGULAR,
-  PLACEHOLDER_SMALL,
-  CTA_BOLD,
-  CTA_SEMI,
-  CAPTION,
-  CAPTION_BOLD,
+  /// The `Heading zero` style
+  ///
+  /// `fontSize: 48`,
+  ///
+  /// `fontWeight: FontWeight.w900`,
+  headingZero,
+
+  /// The `Heading one` style
+  ///
+  /// `fontSize: 36`,
+  ///
+  /// `fontWeight: FontWeight.w900`,
+  headingOne,
+
+  /// The `Heading two` style
+  ///
+  /// `fontSize: 24`,
+  ///
+  /// `fontWeight: FontWeight.w900`,
+  headingTwo,
+
+  /// The `Heading three` style
+  ///
+  /// `fontSize: 20`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  headingThree,
+
+  /// The `Heading four` style
+  ///
+  /// `fontSize: 25`,
+  ///
+  /// `fontWeight: FontWeight.w700`,
+  headingFour,
+
+  /// The `Heading five` style
+  ///
+  /// `fontSize: 14`,
+  ///
+  /// `fontWeight: FontWeight.w700`,
+  headingFive,
+
+  /// The `body large bold` style
+  ///
+  /// `fontSize: 20`,
+  ///
+  /// `fontWeight: FontWeight.w600`,
+  bodyLargeBold,
+
+  /// The `body large regular` style
+  ///
+  /// `fontSize: 20`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  bodyLargeRegular,
+
+  /// The `body medium bold` style
+  ///
+  /// `fontSize: 16`,
+  ///
+  /// `fontWeight: FontWeight.w600`,
+  bodyMediumBold,
+
+  /// The `body medium regular` style
+  ///
+  /// `fontSize: 16`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  bodyMediumRegular,
+
+  /// The `body small bold` style
+  ///
+  /// `fontSize: 14`,
+  ///
+  /// `fontWeight: FontWeight.w600`,
+  bodySmallBold,
+
+  /// The `body small regular` style:
+  ///
+  /// `fontSize: 14`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  bodySmallRegular,
+
+  /// The `body placehold small` style
+  ///
+  /// `fontSize: 14`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  placeholdSmall,
+
+  /// The `call to action bold` style
+  ///
+  /// `fontSize: 12`,
+  ///
+  /// `fontWeight: FontWeight.w700`,
+  callToActionBold,
+
+  /// The `call to action semi` style
+  ///
+  /// `fontSize: 12`,
+  ///
+  /// `fontWeight: FontWeight.w600`,
+  callToActionSemi,
+
+  /// The `caption` style
+  ///
+  /// `fontSize: 12`,
+  ///
+  /// `fontWeight: FontWeight.w400`,
+  caption,
+
+  /// The `caption` style
+  ///
+  /// `fontSize: 12`,
+  ///
+  /// `fontWeight: FontWeight.w500`,
+  captionBold,
 }
