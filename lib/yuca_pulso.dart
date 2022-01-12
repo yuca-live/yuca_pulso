87094400e07f8982cc77fb1c1605d1978fff433a
Will Xavier <willyelns@yuca.live>
2022-01-12 15:49:23 -0300

    fix: snackbar file name

--- lib/yuca_pulso.dart
+++ lib/yuca_pulso.dart
@@ -2,8 +2,7 @@ library yuca_pulso;
 
 export './colors/color_styles.dart';
 export './icons/yuca_icons.dart';
-export './snackbar/yuca_snackbart.dart';
-export './snackbar/yuca_snackbart.dart';
+export './snackbar/yuca_snackbar.dart';
 export './typography/typography.dart';
 export './utils/utils.dart';
 export './widgets/widgets.dart';
