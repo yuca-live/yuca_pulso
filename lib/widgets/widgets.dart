89f5b045f0278873b79530f110a907c99596b6d4
Rodrigo-yuca <rodrigo.amancio@yuca.live>
2022-02-23 15:00:26 -0300

    create appbar, and variation to help button

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export './yuca_divider.dart';
 export './yuca_alert_message.dart';
 export './yuca_button.dart';
 export './yuca_animated_button.dart';
+export './yuca_app_bar.dart';
---
+++ lib/widgets/yuca_app_bar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:yuca_pulso/yuca_pulso.dart';
+
+class YucaAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool allowBackIcon;
+  final List<Widget>? actions;
+  final Widget? leading;
+  final bool withCloseIcon;
+  final Color backgroundColor;
+  final Color titleColor;
+  final Color closeIconColor;
+  final VoidCallback? backButtonFunction;
+  final VoidCallback? closeButtonFunction;
+  final BuildContext context;
+  final dynamic pop;
+  final VoidCallback? helpOnTap;
+  final bool withHelpIcon;
+
+  const YucaAppBar({
+    Key? key,
+    this.title = '',
+    this.allowBackIcon = true,
+    this.actions,
+    this.withCloseIcon = false,
+    this.backgroundColor = YucaColorStyles.base0,
+    this.titleColor = YucaColorStyles.alternativePrimaryDarkest,
+    this.closeIconColor = YucaColorStyles.alternativePrimaryDarkest,
+    this.backButtonFunction,
+    this.closeButtonFunction,
+    this.leading,
+    required this.context,
+    this.pop,
+    this.helpOnTap,
+    this.withHelpIcon = false,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  List<Widget> get _actionsWidgetList {
+    final actionList = actions ?? [];
+    if (withCloseIcon) {
+      actionList.add(Container(
+        padding: const EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: Icon(Icons.close, color: closeIconColor),
+          onPressed: _onTapClose,
+        ),
+      ));
+    }
+    if (withHelpIcon) {
+      actionList.add(GestureDetector(
+        onTap: helpOnTap,
+        child: Container(
+          padding: const EdgeInsets.only(right: 20),
+          child: const Icon(
+            YucaIcons.help,
+            color: YucaColorStyles.base100,
+          ),
+        ),
+      ));
+    }
+    return actionList;
+  }
+
+  Widget get _backButton {
+    return Container(
+      padding: const EdgeInsets.only(left: 20),
+      child: IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: _onTapBack,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: allowBackIcon ? _backButton : leading ?? const SizedBox.shrink(),
+      automaticallyImplyLeading: false,
+      backgroundColor: backgroundColor,
+      elevation: 0.0,
+      centerTitle: true,
+      title: Text(title, style: YucaTypography.create(YucaTextStyles.bodyMediumBold, color: titleColor)),
+      actions: _actionsWidgetList,
+    );
+  }
+
+  void _onTapBack({dynamic pop}) {
+    if (backButtonFunction != null) backButtonFunction!();
+    pop;
+  }
+
+  void _onTapClose({dynamic pop}) {
+    if (closeButtonFunction != null) closeButtonFunction!();
+    pop;
+  }
+}
