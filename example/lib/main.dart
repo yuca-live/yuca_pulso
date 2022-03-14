342f46a8964b2cd48bfe27939cf0915d65d2a0c4
Rodrigo-yuca <rodrigo.amancio@yuca.live>
2022-03-14 13:00:30 -0300

    remove background color from yuca tile

---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:yuca_pulso/yuca_pulso.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 60),
+          YucaTile(
+            title: 'title',
+            onTap: () {
+              print('clique');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
